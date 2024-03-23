9e8e142da7d0c9d26d61629f603c791d52a7c400
carloscardoso05 <cvcardos5@gmail.com>
2024-03-23 11:31:21 -0300

    funções atribuir

--- exercicio_tad/ex01/jogador_de_futebol.go
+++ exercicio_tad/ex01/jogador_de_futebol.go
@@ -1,6 +1,9 @@
 package jogador_de_futebol
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type JogadorDeFutebol struct {
 	nome         string
@@ -20,4 +23,26 @@ func (j *JogadorDeFutebol) Imprime() {
 
 func (j *JogadorDeFutebol) EhBom() bool {
 	return (float64(j.gols)+float64(j.assistencias))/float64(j.jogos) >= 0.6
+}
+
+func (j *JogadorDeFutebol) AtribuirNome(nome string) error {
+	if len(nome) < 3 {
+		msg := fmt.Sprintf("o nome \"%s\" deve conter pelo menos 3 caracteres", nome)
+		return errors.New(msg)
+	}
+	j.nome = nome
+	return nil
+}
+
+func (j *JogadorDeFutebol) AtribuirJogos(jogos uint) {
+	j.jogos = jogos
+
+}
+
+func (j *JogadorDeFutebol) AtribuirPartidas(gols uint) {
+	j.gols = gols
+}
+
+func (j *JogadorDeFutebol) AtribuirAssistencias(assistencias uint) {
+	j.assistencias = assistencias
 }
